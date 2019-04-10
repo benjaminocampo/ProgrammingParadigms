d9f534e1b97a70785e33a784f07b0bb508d986ba
luciano <ton238457@gmail.com>
2019-04-10 13:41:08 -0300

    add dibujo_v and esquina

--- Basico/Escher.hs
+++ Basico/Escher.hs
@@ -24,3 +24,11 @@ dibujo_u p = (^^^) ((^^^) (fish2 p) (Rotar (fish2 p)))
 -- Dibujo t
 dibujo_t :: Dibujo Escher -> Dibujo Escher
 dibujo_t p = (^^^) p ((^^^) (fish2 p) (fish3 p))
+
+-- dibujo v
+dibujo_v p = ciclar (Rotar (dibujo_t p))
+
+-- esquina con nivel de detalle en base a la figura p
+esquina :: Int -> Dibujo Escher -> Dibujo Escher
+esquina 0 p = blanko
+esquina n p = cuarteto (esquina (n-1) p) (lado (n-1) p) (Rotar (lado (n-1) p)) (dibujo_u p) 
